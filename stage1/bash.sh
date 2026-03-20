368b5c9e99a9d4615c44149192bb9d2dea334127
Igniculus Fu <fhh@fhh200000.com>
2026-03-20 23:46:43 +0000

    Update: (not working) bashrc.

--- stage1/bash.sh
+++ stage1/bash.sh
@@ -14,6 +14,7 @@ prebuild() {
 	pushd ..
 	patch -Np1 -i ${SCRIPT_DIR}/bash-disable-stdbool.patch
 	popd
+	CFLAGS="-DSYS_BASHRC='\"/etc/bash.bashrc\"' -DSYS_BASH_LOGOUT='\"/etc/bash.bash_logout\"' -DNON_INTERACTIVE_LOGIN_SHELLS" \
 	../configure --prefix=/usr            \
 		--without-bash-malloc gl_cv_c_bool=y  \
 		--with-installed-readline \
@@ -26,5 +27,37 @@ build() {
 
 install() {
 	make install
+	ret=$?
+	cat > /etc/bash.bashrc <<- "EOF"
+	#
+	# /etc/bash.bashrc
+	#
+
+	# If not running interactively, don't do anything
+	[[ $- != *i* ]] && return
+
+	[[ $DISPLAY ]] && shopt -s checkwinsize
+
+	PS1='[\u@\h \W]\$ '
+
+	case ${TERM} in
+  		xterm*|rxvt*|Eterm|aterm|kterm|gnome*)
+    			PROMPT_COMMAND=${PROMPT_COMMAND:+$PROMPT_COMMAND; }'printf "\033]0;%s@%s:%s\007" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/\~}"'
+
+			;;
+  		screen*)
+ 	   		PROMPT_COMMAND=${PROMPT_COMMAND:+$PROMPT_COMMAND; }'printf "\033_%s@%s:%s\033\\" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/\~}"'
+    			;;
+	esac
+
+	[ -r /usr/share/bash-completion/bash_completion   ] && . /usr/share/bash-completion/bash_completion
+	EOF
+
+	cat > /etc/bash.bash_logout <<- "EOF"
+	#
+	# /etc/bash.bash_logout
+	#
+	EOF
+	return $ret
 }
 
